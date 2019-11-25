9713c6a8d4ee52481bb66a493e22bde004c2c69b
pythonsmasha <52337808+pythonsmasha@users.noreply.github.com>
2019-11-25 12:31:17 -0500

    new database

--- smasha_website/setting.py
+++ smasha_website/setting.py
@@ -2,6 +2,6 @@ import os
 
 import os
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SECRET_KEY = os.environ.get('SECRET KEY')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
