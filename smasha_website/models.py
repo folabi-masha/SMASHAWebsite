ef4e7e5777f709de11377cf3e163867aaeae7f12
pythonsmasha <52337808+pythonsmasha@users.noreply.github.com>
2019-11-26 14:38:18 -0500

    added unique value

--- smasha_website/models.py
+++ smasha_website/models.py
@@ -2,4 +2,4 @@ from .extensions import db
 
 class Emails(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    emails = db.Column(db.String(200), nullable=False)
+    emails = db.Column(db.String(200), nullable=False, unique=True)
